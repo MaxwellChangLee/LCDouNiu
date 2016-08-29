79cfe6a2fd5dc8168e64fb78b57e75c5cb2eed55
dengchenglin <liloveli-2007@163.com>
2016-08-29 19:37:33 +0800

    Initial Commit

---
+++ LCDouNiu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LCDouNiu
+//
+//  Created by Max on 16/8/29.
+//  Copyright © 2016年 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LCDouNiu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LCDouNiu
+//
+//  Created by Max on 16/8/29.
+//  Copyright © 2016年 Max. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
